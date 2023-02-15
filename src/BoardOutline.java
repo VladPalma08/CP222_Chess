03a9f5fb35cf5a4b5117f39aed75f06e0f6f2538
Vlad <vladpalma08@gmail.com>
2023-02-15 16:55:34 -0700

    Testing Commit

---
+++ src/BoardOutline.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class BoardOutline {
+    public void outline() {
+        JPanel p = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                Dimension arcs = new Dimension(100, 100); //Border corners arcs {width,height}, change this to whatever you want
+                int width = getWidth();
+                int height = getHeight();
+                Graphics2D graphics = (Graphics2D) g;
+                graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+
+                //Draws the rounded panel with borders.
+                graphics.setColor(Color.green);
+                graphics.fillRoundRect(0, 0, width - 1, height - 1, arcs.width, arcs.height);//paint background
+                graphics.setColor(Color.green);
+                graphics.drawRoundRect(0, 0, width - 1, height - 1, arcs.width, arcs.height);//paint border
+            }
+        };
+    }
+}
--- src/mainInterface.java
+++ src/mainInterface.java
@@ -1,10 +1,17 @@
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
-import java.util.ArrayList;
+import static javax.swing.JLayeredPane.DEFAULT_LAYER;
 
 public class mainInterface extends JFrame {
 
-    private JPanel mainPanel;
+    JPanel mainPanel;
+    JLayeredPane layeredPane;
+    JPanel mainBoard;
+
+    public void outline() {
+
+    }
 
     public mainInterface() {
         this.setSize(900,900);
@@ -13,55 +20,75 @@ public class mainInterface extends JFrame {
         this.setVisible(true);
         this.setContentPane(mainPanel);
 
-        mainPanel.setBounds(92, 80, 740, 740);
-        mainPanel.setLayout(new GridBagLayout());
-        mainPanel.setVisible(true);
+        //  Use a Layered Pane for this application
+        layeredPane = new JLayeredPane();
+        mainPanel.add(layeredPane);
+        layeredPane.setSize(740,740);
 
-        JPanel[] gridHori = new JPanel[8];
-        JPanel[] gridVerti = new JPanel[8];
+        mainBoard = new JPanel();
+        mainBoard.setLayout(new GridLayout(8, 8));
+        mainBoard.setPreferredSize(layeredPane.getSize());
+        mainBoard.setBounds(0,0,layeredPane.getWidth(), layeredPane.getHeight());
 
-        int forWidth = mainPanel.getWidth()/8;
-        int forHeight = mainPanel.getHeight()/8;
+        Color firstSqaure = Color.decode("#eeeed2");
+        Color secondSquare = Color.decode("#769656");
 
-        for(int i=0; i<gridHori.length; i++) {
-            gridHori[i] = new JPanel();
-            gridHori[i].setSize(forWidth,forHeight);
-            gridHori[i].setBackground(Color.BLACK);
-            mainPanel.add(gridHori[i]);
+        for (int i = 0; i < 8; i++)
+        {
+            for (int j = 0; j < 8; j++)
+            {
+                JPanel boardSquare = new JPanel(new BorderLayout());
+                boardSquare.setBackground((i + j) % 2 == 0 ? firstSqaure : secondSquare);
+                mainBoard.add(boardSquare);
+            }
         }
 
+        layeredPane.add(mainBoard, DEFAULT_LAYER);
+        mainPanel.setBounds(92, 80, 740, 740);
 
-//
-//            JPanel testing = new JPanel();
-//
-//            testing.setSize(forWidth,forHeight);
-//            testing.setBackground(Color.BLACK);
-//
-//            mainPanel.add(testing);
+        mainPanel.setVisible(true);
+
 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
-
-
-//    public void paintComponent(Graphics g) {
-//        Graphics2D g2 = (Graphics2D) g;
-//        g2.drawRect(100, 0, 400, 400);
-//        int z = 2;
-//        int y = 0;
-//        for (int i = 0; i < 8; i++) {
-//            for (int x = 100; x < 550; x += 50) {
-//                z++;
-//                g2.drawRect(x, y, 50, 50);
-//                if ((z % 2) == 0) {
-//                    g2.fillRect(x, y, 50, 50);
-//                }
+//    public void graphics() {
+//
+//        JPanel p = new JPanel() {
+//            protected void paintComponent(Graphics g) {
+//                super.paintComponent(g);
+//                Dimension arcs = new Dimension(100, 100); //Border corners arcs {width,height}, change this to whatever you want
+//                int width = getWidth();
+//                int height = getHeight();
+//                Graphics2D graphics = (Graphics2D) g;
+//                graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+//
+//                //Draws the rounded panel with borders.
+//                graphics.setColor(Color.green);
+//                graphics.fillRoundRect(0, 0, width - 1, height - 1, arcs.width, arcs.height);//paint background
+//                graphics.setColor(Color.green);
+//                graphics.drawRoundRect(0, 0, width - 1, height - 1, arcs.width, arcs.height);//paint border
 //            }
-//            y = y + 50;
-//        }
+//        };
 //    }
 
     public static void main(String[] args) {
         mainInterface test = new mainInterface();
     }
 }
+
+//    // previous logic for creating chess squares
+//    JPanel[] gridHori = new JPanel[4];
+//    JPanel[] gridVerti = new JPanel[4];
+//
+//    int forWidth = mainPanel.getWidth()/8;
+//    int forHeight = mainPanel.getHeight()/8;
+//    int count = 0;
+//
+//        for(int i = 0; i < gridHori.length; i++) {
+//        gridHori[i] = new JPanel();
+//        gridHori[i].setBounds(count,0,forWidth,forHeight);
+//        gridHori[i].setBackground(Color.BLACK);
+//        mainPanel.add(gridHori[i]);
+//        count += (forWidth*2);
+//        }

5c3b2b4ab924760d9f53ac2547634a8154e23189
Vlad <vladpalma08@gmail.com>
2023-02-14 19:13:09 -0700

    Testing Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        String empty = "";
+        empty = "Testing this method";
+        System.out.println(empty);
         System.out.println("Testing my Git repository");
         System.out.println("Hello world!");
     }

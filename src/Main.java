b9433430c542136ab61c67c8d28a3329321fcced
Vlad <vladpalma08@gmail.com>
2023-02-14 19:24:07 -0700

    Testing Commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
 public class Main {
+
+    public static void timeTester() {
+        System.out.println("How long will it take for Git to update the repository through IntelliJ?");
+    }
+
     public static void main(String[] args) {
         String empty = "";
         empty = "Testing this method";
+        timeTester();
         System.out.println(empty);
         System.out.println("Testing my Git repository");
         System.out.println("Hello world!");

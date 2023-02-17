387d71b75b877530a4fb4fc303330e1bc38ecc74
Vlad <vladpalma08@gmail.com>
2023-02-17 01:39:21 -0700

    Testing Commit

--- src/mainInterface.java
+++ src/mainInterface.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.image.BufferedImage;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Objects;
 
 import static javax.swing.JLayeredPane.DEFAULT_LAYER;
@@ -10,19 +11,71 @@ public class mainInterface extends JFrame {
     JPanel mainPanel;
     JLayeredPane layeredPane;
     JPanel mainBoard;
-    ImageIcon img;
-    JLabel background;
-    BufferedImage myPicture;
-    JLabel picLabel;
+    JButton menuButton;
+//    ImageIcon img;
+//    JLabel background;
+//    BufferedImage myPicture;
+//    JLabel picLabel;
+
+//    Method for creating buttons:
+//    public void createButton(JButton button) {
+//        button.setFont(new Font("Tahoma", Font.ITALIC, 11));
+//        button.setForeground(Color.white);
+//        button.setBackground(Color.decode("#4c4c4c"));
+//        button.setSize(90,20);
+//        button.setContentAreaFilled(false);
+//        button.setBorderPainted(false);
+//        button.setVisible(true);
+//        this.add(button);
+//    }
+
+    public void createMenuButton() {
+        JButton button = new JButton();
+        button.setFont(new Font("Tahoma", Font.ITALIC, 11));
+        button.setForeground(Color.white);
+        button.setBackground(Color.decode("#4c4c4c"));
+        button.setSize(90,20);
+        button.setContentAreaFilled(false);
+        button.setBorderPainted(false);
+        button.setVisible(true);
+
+        button.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                System.exit(0);
+            }
+        });
+
+        this.add(button);
+    }
+
+    public void createNewButton() {
+        JButton button2 = new JButton();
+        button2.setFont(new Font("Tahoma", Font.ITALIC, 11));
+        button2.setForeground(Color.white);
+        button2.setBackground(Color.decode("#4c4c4c"));
+        button2.setSize(110,20);
+        button2.setBounds(90,0,110,20);
+        button2.setContentAreaFilled(false);
+        button2.setBorderPainted(false);
+        button2.setVisible(true);
+
+        button2.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                System.exit(0);
+            }
+        });
+
+        this.add(button2);
+    }
 
     public mainInterface() {
-        this.setSize(900,900);
+        this.setSize(900,920);
         this.setLocationRelativeTo(null);
         this.setResizable(false);
 
         ImageIcon img = new ImageIcon(Objects.requireNonNull(getClass().getClassLoader().getResource("chess.jpg")));
         JLabel background = new JLabel("", img, JLabel.CENTER);
-        background.setBounds(0, 0, 900, 900);
+        background.setBounds(0, 50, 900, 990);
 
         this.setVisible(true);
         this.setContentPane(background);
@@ -32,29 +85,6 @@ public class mainInterface extends JFrame {
         mainPanel.add(layeredPane);
         mainPanel.setLayout(new BorderLayout());
         layeredPane.setSize(720,720);
-
-        JButton button = new JButton("Menu");
-        button.setFont(new Font("Tahoma", Font.ITALIC, 11));
-        button.setForeground(Color.white);
-        button.setBackground(Color.decode("#4c4c4c"));
-        button.setSize(90,20);
-        button.setOpaque(false);
-        button.setContentAreaFilled(false);
-        button.setBorderPainted(false);
-        button.setVisible(true);
-        this.add(button);
-
-        JButton button2 = new JButton("New Game");
-        button2.setFont(new Font("Tahoma", Font.ITALIC, 11));
-        button2.setForeground(Color.white);
-        button2.setBackground(Color.decode("#4c4c4c"));
-        button2.setSize(110,20);
-        button2.setBounds(90,0,110,20);
-        button2.setOpaque(false);
-        button2.setContentAreaFilled(false);
-        button2.setBorderPainted(false);
-        button2.setVisible(true);
-        this.add(button2);
 
         mainBoard = new JPanel();
 
@@ -73,26 +103,18 @@ public class mainInterface extends JFrame {
             }
         }
 
-//        mainBoard.getComponent( 15 ).setBackground(Color.blue);
-//        mainBoard.getComponent( 50 ).setBackground(Color.orange);
-//        mainBoard.getComponent( 26 ).setBackground(Color.pink);
-
         layeredPane.add(mainBoard, DEFAULT_LAYER);
-        mainPanel.setBounds(98, 79, 720, 720);
+        mainPanel.setBounds(98, 80, 720, 720);
 
         // add pieces to individual squares
         ImageIcon duke = new ImageIcon(Objects.requireNonNull(getClass().getClassLoader().getResource("kb.svg")));
         JLabel image = new JLabel("", duke, JLabel.CENTER);
         image.setBounds(0, 0, 90, 90);
 
-        // change index (currently 0) to access a specific square
-//        JPanel panel = (JPanel) mainBoard.getComponent( 0 );
-//        panel.setBackground(Color.BLUE);
-
-        // add the image to the panel
-//        panel.add(image);
-
         mainPanel.setVisible(true);
+
+        createMenuButton();
+        createNewButton();
 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
@@ -102,7 +124,16 @@ public class mainInterface extends JFrame {
     }
 }
 
-//    // previous logic for creating chess squares
+//    mainBoard.getComponent( 15 ).setBackground(Color.blue);
+//    mainBoard.getComponent( 50 ).setBackground(Color.orange);
+//    mainBoard.getComponent( 26 ).setBackground(Color.pink);
+
+//    change index (currently 0) to access a specific square
+//    JPanel panel = (JPanel) mainBoard.getComponent( 0 );
+//    panel.setBackground(Color.BLUE);
+//    panel.add(image);
+
+//    previous logic for creating chess squares
 //    JPanel[] gridHori = new JPanel[4];
 //    JPanel[] gridVerti = new JPanel[4];
 //
@@ -117,5 +148,3 @@ public class mainInterface extends JFrame {
 //        mainPanel.add(gridHori[i]);
 //        count += (forWidth*2);
 //        }
-
-
